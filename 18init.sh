846d440898ef78636f17dfc9f2716368e486405a
b-lamer <152201467+b-lamer@users.noreply.github.com>
2025-02-09 18:50:47 -0600

    :)

--- 18init.sh
+++ 18init.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
-mkdir -p CCDC
-cd CCDC
-mkdir -p Scripts
+systemctl stop sshd
+systemctl disable sshd
+systemctl mask sshd
 
-#Add more wget statements with which scripts are being implemented
+chown -R www-data:www-data /var/www/html
+chmod -R 750 /var/www/html
+
+a2dismod -f autoindex status
+systemctl restart apache2
+systemctl restart httpd
+
+
+
+
+
+#Scripts
+mkdir -p Scripts
 cd Scripts
-wget https://raw.githubusercontent.com/b-lamer/temprepo/main/skripts/test1.sh
+wget https://raw.githubusercontent.com/uwwisaca/temprepo/refs/heads/main/Scripts/iptables.sh
 
