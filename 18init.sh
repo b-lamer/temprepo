0f49c3198c3931406cb34cb6f8975abb43792cd8
b-lamer <152201467+b-lamer@users.noreply.github.com>
2025-02-10 11:07:09 -0600

    Update 18init.sh

--- 18init.sh
+++ 18init.sh
@@ -6,13 +6,13 @@ systemctl mask sshd
 
 chown -R www-data:www-data /var/www/html
 chmod -R 750 /var/www/html
+chmod 640 /etc/apache2/apache2.conf
+chmod 640 /etc/apache2/sites-available/*
+chmod 640 /var/log/apache2/*
 
 a2dismod -f autoindex status
 systemctl restart apache2
 systemctl restart httpd
-
-
-
 
 
 #Scripts
